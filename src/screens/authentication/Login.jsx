a0f8302e85dcf15a0c4dd9dc288471bb130a04d0
hemashreeb <hhemashreebalraj@clarku.edu>
2023-11-13 22:41:18 +0530

    initial login screen setup

--- src/screens/authentication/Login.jsx
+++ src/screens/authentication/Login.jsx
@@ -1,12 +1,52 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {
+  SafeAreaView,
+  Text,
+  View,
+  TextInput,
+  Button,
+  StyleSheet,
+} from 'react-native';
 
 const Login = () => {
+  //const [value, setValue] = useState(0);
+  const submitForm = () => {
+    console.log('Form Submitted');
+  };
+
   return (
-    <SafeAreaView>
-      <Text>Login</Text>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text style={styles.header}>Login Form</Text>
+      </View>
+      <TextInput style={styles.input} placeholder="Enter Email" />
+      <TextInput
+        style={styles.input}
+        secureTextEntry={true}
+        placeholder="Enter Password"
+      />
+      <Button style={styles.button} title="Submit" onPress={submitForm} />
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 100,
+    display: 'flex',
+  },
+  header: {
+    fontSize: 40,
+    alignSelf: 'center',
+  },
+  input: {
+    paddingTop: 50,
+    paddingLeft: 30,
+    paddingRight: 30,
+  },
+  button: {
+    backgroundColor: 'blue',
+  },
+});
 
 export default Login;
