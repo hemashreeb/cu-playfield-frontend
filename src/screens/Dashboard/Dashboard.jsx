1e7f50a0b5a6d2577ea09b51c70e0001da71e3fb
hemashreeb <hhemashreebalraj@clarku.edu>
2023-11-16 12:53:39 +0530

    init dashboard

--- src/screens/Dashboard/Dashboard.jsx
+++ src/screens/Dashboard/Dashboard.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {SafeAreaView, Text, StyleSheet} from 'react-native';
 
 const Dashboard = () => {
   return (
-    <SafeAreaView>
-      <Text>Dashboard</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.header}>Dashboard</Text>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  header: {
+    fontSize: 30,
+  },
+});
 
 export default Dashboard;
